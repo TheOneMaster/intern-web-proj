c7d042c7a819f4dd72dc8af1074b342edfb16363
TheOneMaster <25909677+TheOneMaster@users.noreply.github.com>
2024-04-13 03:06:57 +0530

    Added details to recommended event card

--- src/components/RecommendedEvent.tsx
+++ src/components/RecommendedEvent.tsx
@@ -5,19 +5,35 @@ import PlaceholderImage from "../assets/Rectangle 2.svg"
 type RecommendedEventProps = EventData
 
 export function RecommendedEventCard(props: RecommendedEventProps) {
+
+    const date = new Date(props.date);
+    const distance = parseInt(props.distanceKm);
+
+    const dateStr = date.toLocaleDateString("en-US", {
+        month: "short",
+        day: "numeric",
+        year: "numeric"
+    })
+
     return (
-        <div className="relative">
+        <div className="relative drop-shadow-md">
             <img
                 src={PlaceholderImage}
-                className="bg-cover min-w-60"
+                className="bg-cover min-w-[19rem]"
                 />
-            <div className="absolute">
-                <div>
-                    <span>{props.eventName}</span>
-                    <span>
-                        <i className="i-geo-alt-fill"></i>
-                        {props.cityName}
-                    </span>
+            <div className="absolute bottom-2 px-2">
+                <div className="flex flex-row">
+                    <div>
+                        <p className="text-white font-bold text-xl">{props.eventName}</p>
+                        <p className="text-subtitle text-sm">
+                            <i className="bi-geo-alt-fill mr-1"></i>
+                            {props.cityName}
+                        </p>
+                    </div>
+                    <div className="text-subtitle text-sm flex flex-col mt-1">
+                        <p className="whitespace-nowrap">{dateStr}</p>
+                        <p className="mt-auto">{props.weather} | {distance} Km</p>
+                    </div>
                 </div>
             </div>
 
